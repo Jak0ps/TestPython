654d7068536b2739be8e81134dec6247b6c1fe73
Jacob Saleh <jacob.saleh@hpe.com>
2018-01-02 14:52:28 +0200

    random exercise 5

--- Random exercises.py
+++ Random exercises.py
@@ -31,3 +31,48 @@ def digit_sum2(n):
 
 digit_sum2(1234)
 
+#Exercise 3
+def factorial(x):
+    sum = 1
+    while x > 0:
+        sum *= x
+        x -= 1
+    return sum
+
+print factorial(4)
+print factorial(1)
+print factorial(3)
+
+#Exercise 4
+def is_prime(x):
+    if x < 2:
+        return False
+    for n in range(2,x):
+        if float(x) % n == 0.0:
+            return False
+            break
+    return True
+
+for i in range(18):
+    print "prime of", i, is_prime(i)
+
+#Exercise 5
+def reverse(text):
+    rev_text = []
+    for i in str(text):
+        rev_text.append(i)
+    lenoftext = len(rev_text)
+    while lenoftext > 0:
+        print rev_text[lenoftext-1],
+        rev_text.pop(lenoftext-1)
+        lenoftext -= 1
+
+#Correct code
+
+def reverse2(text):
+    word = ""
+    l = len(text) - 1
+    while l >= 0:
+        word = word + text[l]
+        l -= 1
+    return word
