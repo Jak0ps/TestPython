846282b3ca8cc796a4160dd3b6b52ea039c6de9b
Jacob Saleh <jacob.saleh@hpe.com>
2018-01-06 11:46:54 +0200

    EXAM STATISTICS Completed

--- Exam_stats.py
+++ Exam_stats.py
@@ -1,23 +1,60 @@
 #!/usr/bin/python
 
+"""
+My code
 grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
 
 def print_grades(grades_input):
     for i in grades_input:
         print i
 
-print print_grades(grades)
-
 def grades_sum(scores):
     total = 0
     for i in scores:
-        total = total + i
+        total += i
     return total
-
-print grades_sum(grades)
 
 def grades_average(grades_input):
 #    return ("%.2f" % (grades_sum(grades_input) / float(len(grades_input))))
     return grades_sum(grades_input) / float(len(grades_input))
 
+#Code of CodeCademy
+"""
+
+grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+
+def print_grades(grades_input):
+    for grade in grades_input:
+        print grade
+
+def grades_sum(scores):
+    total = 0
+    for score in scores:
+        total += score
+    return total
+
+def grades_average(grades_input):
+    sum_of_grades = grades_sum(grades_input)
+    average = sum_of_grades / float(len(grades_input))
+    return average
+
+def grades_variance(scores):
+    average = grades_average(scores)
+    variance = 0
+    for score in scores:
+        variance += ((average - score) ** 2)
+    return variance / len(scores)
+
+print grades_variance(grades)
+
+def grades_std_deviation(variance):
+    return variance ** 0.5
+
+variance = grades_variance(grades)
+print grades_std_deviation(variance)
+
+print print_grades(grades)
+print grades_sum(grades)
 print grades_average(grades)
+print grades_variance(grades)
+print grades_std_deviation(grades_variance(grades))
