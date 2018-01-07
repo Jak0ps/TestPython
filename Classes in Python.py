302d5c1bef401d981061735e80b30ad77656e50c
Jacob Saleh <jacob.saleh@hpe.com>
2018-01-07 18:34:40 +0200

    Classes 123

--- Classes in Python.py
+++ Classes in Python.py
@@ -110,3 +110,130 @@ class Animal(object):
 
 hippo = Animal("Johny",6)
 hippo.description()
+
+
+#Class Variables
+class Animal(object):
+    """Makes cute animals."""
+    is_alive = True
+    health = "good"
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        # Add your method here!
+    def description(self):
+        print self.name
+        print self.age
+
+hippo = Animal("Johny",6)
+sloth = Animal("Jo",8)
+ocelot = Animal("Jee",14)
+print hippo.health
+print sloth.health
+print ocelot.health
+
+#Real world code
+class ShoppingCart(object):
+    """Creates shopping cart objects
+    for users of our fine website."""
+    items_in_cart = {}
+    def __init__(self, customer_name):
+        self.customer_name = customer_name
+
+    def add_item(self, product, price):
+        """Add product to the cart."""
+        if not product in self.items_in_cart:
+            self.items_in_cart[product] = price
+            print product + " added."
+        else:
+            print product + " is already in the cart."
+
+    def remove_item(self, product):
+        """Remove product from the cart."""
+        if product in self.items_in_cart:
+            del self.items_in_cart[product]
+            print product + " removed."
+        else:
+            print product + " is not in the cart."
+#Adding Banana to my shopping cart
+my_cart = ShoppingCart("Jacob")
+my_cart.add_item("Banana",14)
+
+#Class Inheritance , look at the class name and the "object" of the 2nd class
+class Customer(object):
+    """Produces objects that represent customers."""
+    def __init__(self, customer_id):
+        self.customer_id = customer_id
+
+    def display_cart(self):
+        print "I'm a string that stands in for the contents of your shopping cart!"
+
+class ReturningCustomer(Customer):
+    """For customers of the repeat variety."""
+    def display_order_history(self):
+        print "I'm a string that stands in for your order history!"
+
+monty_python = ReturningCustomer("ID: 12345")
+monty_python.display_cart()
+monty_python.display_order_history()
+
+#Inheritance syntax
+"""
+In Python, inheritance works like this:
+
+class DerivedClass(BaseClass):
+  # code goes here
+  
+where DerivedClass is the new class you're making and BaseClass is the class from which that new class inherits.
+"""
+
+class Shape(object):
+    """Makes shapes!"""
+    def __init__(self, number_of_sides):
+        self.number_of_sides = number_of_sides
+
+class Triangle(Shape):
+    def __init__(self, side1, side2, side3):
+        self.side1 = side1
+        self.side2 = side2
+        self.side3 = side3
+
+#Override in class
+class Employee(object):
+    def __init__(self, name):
+        self.name = name
+    def greet(self, other):
+        print "Hello, %s" % other.name
+
+class CEO(Employee):
+    def greet(self, other):
+        print "Get back to work, %s!" % other.name
+
+ceo = CEO("Emily")
+emp = Employee("Steve")
+emp.greet(ceo)
+# Hello, Emily
+ceo.greet(emp)
+# Get back to work, Steve!
+
+#Example
+class Employee(object):
+    """Models real-life employees!"""
+    def __init__(self, employee_name):
+        self.employee_name = employee_name
+
+    def calculate_wage(self, hours):
+        self.hours = hours
+        return hours * 20.00
+
+class PartTimeEmployee(Employee):
+    def calculate_wage(self, hours):
+        self.hours = hours
+        return hours * 12.00
+    def full_time_wage(self, hours):
+        return super(PartTimeEmployee, self).calculate_wage(hours)
+
+milton = PartTimeEmployee("Jacob")
+print milton.full_time_wage(10)
+
+
