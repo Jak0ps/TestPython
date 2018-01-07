e3c71722af8ed478147d4d2c03a48deea1da5c6c
Jacob Saleh <jacob.saleh@hpe.com>
2018-01-07 16:25:31 +0200

    Classes 123

--- Classes in Python.py
+++ Classes in Python.py
@@ -44,3 +44,69 @@ worker.is_manager()
 mangr.print_welcome()
 mangr.is_manager()
 
+#Class Syntax
+#This is an empty class
+class Animal(object):
+    pass
+
+#Animal Class
+class Animal(object):
+    def __init__(self, name):
+        self.name = name
+zebra = Animal("Jeffrey")
+print zebra.name
+
+#Example
+# Class definition
+class Animal(object):
+    """Makes cute animals."""
+    # For initializing our instance objects
+    def __init__(self, name, age, is_hungry):
+        self.name = name
+        self.age = age
+        self.is_hungry = is_hungry
+
+# Note that self is only used in the __init__()
+# function definition; we don't need to pass it
+# to our instance objects.
+
+zebra = Animal("Jeffrey", 2, True)
+giraffe = Animal("Bruce", 1, False)
+panda = Animal("Chad", 7, True)
+
+print zebra.name, zebra.age, zebra.is_hungry
+print giraffe.name, giraffe.age, giraffe.is_hungry
+print panda.name, panda.age, panda.is_hungry
+
+#Class Scope
+class Animal(object):
+    """Makes cute animals."""
+    is_alive = True
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+zebra = Animal("Jeffrey", 2)
+giraffe = Animal("Bruce", 1)
+panda = Animal("Chad", 7)
+
+print zebra.name, zebra.age, zebra.is_alive
+print giraffe.name, giraffe.age, giraffe.is_alive
+print panda.name, panda.age, panda.is_alive
+
+
+#Example
+
+class Animal(object):
+    """Makes cute animals."""
+    is_alive = True
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        # Add your method here!
+    def description(self):
+        print self.name
+        print self.age
+
+hippo = Animal("Johny",6)
+hippo.description()
